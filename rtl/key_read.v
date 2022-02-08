2af524ede20455f3c28ab31b4a446435cb8c002a
Markos Stefanidis <markos.stefanidhs@gmail.com>
2022-02-08 12:04:20 +0200

    Clean up code

--- rtl/key_read.v
+++ rtl/key_read.v
@@ -3,15 +3,15 @@ module key_read(
 	input rst_n,
 	input conf_en,
 	input [3:0] row,
-	
+
 	output [2:0] col,
 	output reg [7:0] conf_addr,
 	output reg [7:0] conf_data,
 	output reg sccb_start,
-	
+
 	output reg take_pic,
 	output reg hdr_en,
-	
+
 	output reg seven_seg_right,
 	output reg seven_seg_left,
 	output  seven_seg_A,
@@ -22,20 +22,20 @@ module key_read(
 	output  seven_seg_F,
 	output  seven_seg_G,
 	output  seven_seg_DP,
-	
+
 	output [2:0] state_led,
 	output addr_led
 );
-	
+
 	localparam IDLE = 0;
 	localparam WAIT_1_DIGIT = 1;
 	localparam WAIT_2_DIGIT = 2;
 	localparam WAIT_3_DIGIT = 3;
 	localparam CONFIRM = 4;
-	
+
 	localparam CLK_FREQ = 25000000;
 	localparam SEVEN_SEG_FREQ = 50;
-	
+
 	reg [2:0] STATE;
 	reg scan;
 	reg [7:0] data;
@@ -58,10 +58,9 @@ module key_read(
 			hdr_en <= 1'b0;
 		end else begin
 			q_key_ready <= key_ready;
-		
-		
+
 			case(STATE)
-			
+
 				IDLE: begin
 					if(conf_en) begin
 						STATE <= WAIT_1_DIGIT;
@@ -70,43 +69,43 @@ module key_read(
 						scan <= 1'b0;
 					end
 					sccb_start <= 1'b0;
-					
+
 					if(key_ready && ~q_key_ready) begin
 						case(key_code)
 							4'hA: take_pic <= ~take_pic;
 							4'hB: hdr_en <= ~hdr_en;
 						endcase
 					end
-					
+
 				end
-				
+
 				WAIT_1_DIGIT: begin
 					sccb_start <= 1'b0;
 					if(key_ready && ~q_key_ready) begin
 						case(key_code)
-							
+
 							4'b0010: begin
 								data <= 8'hC8; //Key 2*100
 								STATE <= WAIT_2_DIGIT;
 							end
-							
+
 							4'b0001: begin
 								data <= 8'h64; //Key 1*100
 								STATE <= WAIT_2_DIGIT;
 							end
-							
+
 							4'b0000: begin
 								data <= 8'h0;  //Key 0*100
 								STATE <= WAIT_2_DIGIT;
 							end
-							
+
 							default: begin
 								data <= 8'h0;
 								STATE <= WAIT_1_DIGIT;
 							end
-							
+
 						endcase
-						scan <= 1'b1;						
+						scan <= 1'b1;
 					end else if (~conf_en) begin
 						STATE <= IDLE;
 						scan <= 1'b0;
@@ -114,10 +113,10 @@ module key_read(
 						scan <= 1'b0;
 					end
 				end
-				
+
 				WAIT_2_DIGIT: begin
 					if(key_ready && ~q_key_ready) begin
-						case(key_code)								
+						case(key_code)
 							4'b1001: begin
 								if(data == 8'hC8) begin
 									STATE <= WAIT_2_DIGIT;
@@ -126,7 +125,7 @@ module key_read(
 									STATE <= WAIT_3_DIGIT;
 								end
 							end
-							
+
 							4'b1000: begin
 								if(data == 8'hC8) begin
 									STATE <= WAIT_2_DIGIT;
@@ -135,7 +134,7 @@ module key_read(
 									STATE <= WAIT_3_DIGIT;
 								end
 							end
-							
+
 							4'b0111: begin
 								if(data == 8'hC8) begin
 									STATE <= WAIT_2_DIGIT;
@@ -144,7 +143,7 @@ module key_read(
 									STATE <= WAIT_3_DIGIT;
 								end
 							end
-							
+
 							4'b0110: begin
 								if(data == 8'hC8) begin
 									STATE <= WAIT_2_DIGIT;
@@ -153,42 +152,42 @@ module key_read(
 									STATE <= WAIT_3_DIGIT;
 								end
 							end
-							
+
 							4'b0101: begin
 								data <= data + 8'h32; //Key 5*10
 								STATE <= WAIT_3_DIGIT;
 							end
-							
+
 							4'b0100: begin
 								data <= data + 8'h28; //Key 4*10
 								STATE <= WAIT_3_DIGIT;
 							end
-							
+
 							4'b0011: begin
 								data <= data + 8'h1E; //Key 3*10
 								STATE <= WAIT_3_DIGIT;
 							end
-							
+
 							4'b0010: begin
 								data <= data + 8'h14; //Key 2*10
 								STATE <= WAIT_3_DIGIT;
 							end
-							
+
 							4'b0001: begin
 								data <= data + 8'hA; //Key 1*10
 								STATE <= WAIT_3_DIGIT;
 							end
-							
+
 							4'b0000: begin
 								data <= data; 		  //Key 0*10
 								STATE <= WAIT_3_DIGIT;
 							end
-							
+
 							4'b1010: begin
 								data <= 8'b0;
 								STATE <= WAIT_1_DIGIT;
 							end
-							
+
 							default: begin
 								STATE <= WAIT_2_DIGIT;
 							end
@@ -202,13 +201,13 @@ module key_read(
 						scan <= 1'b0;
 					end
 				end
-				
-				
-				
-				
+
+
+
+
 				WAIT_3_DIGIT: begin
 					if(key_ready && ~q_key_ready) begin
-						case(key_code)					
+						case(key_code)
 							4'b1001: begin
 								if(data == 8'hFA) begin
 									STATE <= WAIT_3_DIGIT;
@@ -216,9 +215,9 @@ module key_read(
 									data <= data + 8'h9; //Key 9
 									STATE <= CONFIRM;
 								end
-								
+
 							end
-							
+
 							4'b1000: begin
 								if(data == 8'hFA) begin
 									STATE <= WAIT_3_DIGIT;
@@ -227,7 +226,7 @@ module key_read(
 									STATE <= CONFIRM;
 								end
 							end
-							
+
 							4'b0111: begin
 								if(data == 8'hFA) begin
 									STATE <= WAIT_3_DIGIT;
@@ -236,7 +235,7 @@ module key_read(
 									STATE <= CONFIRM;
 								end
 							end
-							
+
 							4'b0110: begin
 								if(data == 8'hFA) begin
 									STATE <= WAIT_3_DIGIT;
@@ -245,42 +244,42 @@ module key_read(
 									STATE <= CONFIRM;
 								end
 							end
-							
+
 							4'b0101: begin
 								data <= data + 8'h5; //Key 5
 								STATE <= CONFIRM;
 							end
-							
+
 							4'b0100: begin
 								data <= data + 8'h4; //Key 4
 								STATE <= CONFIRM;
 							end
-							
+
 							4'b0011: begin
 								data <= data + 8'h3; //Key 3
 								STATE <= CONFIRM;
 							end
-							
+
 							4'b0010: begin
 								data <= data + 8'h2; //Key 2
 								STATE <= CONFIRM;
 							end
-							
+
 							4'b0001: begin
 								data <= data + 8'h1; //Key 1
 								STATE <= CONFIRM;
 							end
-							
+
 							4'b0000: begin
 								data <= data; 		  //Key 0
 								STATE <= CONFIRM;
 							end
-							
+
 							4'b1010: begin
 								data <= 8'b0;
 								STATE <= WAIT_1_DIGIT;
 							end
-							
+
 							default: begin
 								STATE <= WAIT_3_DIGIT;
 							end
@@ -293,9 +292,9 @@ module key_read(
 						scan <= 1'b0;
 					end
 				end
-				
-			
-				
+
+
+
 				CONFIRM: begin
 					if(key_ready && ~q_key_ready) begin
 						case(key_code)
@@ -311,11 +310,11 @@ module key_read(
 								STATE <= WAIT_1_DIGIT;
 								scan <= 1'b1;
 							end
-							
+
 							4'b1010: begin
 								STATE <= WAIT_1_DIGIT;
 								scan <= 1'b1;
-							end							
+							end
 						endcase
 					end else if (~conf_en) begin
 						STATE <= IDLE;
@@ -323,29 +322,29 @@ module key_read(
 					end else begin
 						scan <= 1'b0;
 					end
-				end				
-			
+				end
+
 			endcase
-		
+
 		end
-	
+
 	end
-	
-	
+
+
 	key_pad key_pad(
 		.clk (clk),
 		.rst_n (rst_n),
 		.scan (scan),
-		.row (row),	
-		
+		.row (row),
+
 		.key_code (key_code),
 		.col (col),
 		.key_ready (key_ready)
 	);
-	
+
 	reg [17:0] seven_seg_counter;
 	wire [3:0] num;
-	
+
 	always@(posedge clk) begin
 		if(~rst_n) begin
 			seven_seg_counter <= 18'b0;
@@ -357,12 +356,12 @@ module key_read(
 			seven_seg_left <= (seven_seg_counter == (CLK_FREQ/(2*SEVEN_SEG_FREQ) - 1)) ? ~seven_seg_left : seven_seg_left;
 		end
 	end
-	
+
 	assign num = (seven_seg_left) ? data[3:0] : data[7:4];
-	
+
 	seven_seg seven_seg(
 		.num (num),
-		
+
 		.A  (seven_seg_A),
 		.B  (seven_seg_B),
 		.C  (seven_seg_C),
@@ -372,7 +371,7 @@ module key_read(
 		.G  (seven_seg_G),
 		.DP (seven_seg_DP)
 	);
-	
+
 	assign  state_led = {(STATE != WAIT_1_DIGIT), (STATE != WAIT_2_DIGIT), (STATE != WAIT_3_DIGIT)} ;
 	assign addr_led = ~wr_addr;
 
