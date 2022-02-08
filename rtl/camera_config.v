6dc1ce2904d730a6fcf7f6c9fedc7071ef590b9a
Markos Stefanidis <markos.stefanidhs@gmail.com>
2022-02-08 12:05:26 +0200

    Fix exposure order

--- rtl/camera_config.v
+++ rtl/camera_config.v
@@ -7,7 +7,7 @@ module camera_config(
 	input change_exp,
 	input hdr_en,
 	input [2:0] last_frame,
-	
+
 	output sda,
 	output scl,
 	output done
@@ -15,21 +15,21 @@ module camera_config(
 
 	// This module is responsible for configuring the camera module.
 	// ov7670_config module feeds the sccb interface with the address of the configuration register and the data to be written.
-	// On reset, the ov7670_config module reads the address and the data from the ov7670_rom. The camera can be configured manually through a keypad. 
+	// On reset, the ov7670_config module reads the address and the data from the ov7670_rom. The camera can be configured manually through a keypad.
 
 	localparam IDLE = 0;
 	localparam CHANGE_EXP1 = 1;
 	localparam WAIT_STATE = 2;
 	localparam CHANGE_EXP2 = 3;
-	
-	
+
+
 	wire [7:0] rom_address;
 	wire [15:0] rom_data;
 	wire sccb_ready;
 	wire sccb_start;
 	wire [7:0] sccb_data;
-	wire [7:0] sccb_address;	
-	
+	wire [7:0] sccb_address;
+
 	reg [1:0] STATE;
 
 	reg [7:0] conf_data;
@@ -38,7 +38,7 @@ module camera_config(
 	reg [1:0] exposure;
 	reg q_hdr_en;
 	reg q_start;
-	
+
 	always@(posedge clk_25M) begin
 		if(~rst_n) begin
 			STATE <= IDLE;
@@ -68,30 +68,30 @@ module camera_config(
 					end else begin
 						config_start <= 1'b0;
 					end
-				end		
-				
+				end
+
 				CHANGE_EXP1: begin
-					if(done) begin	
+					if(done) begin
 						if(last_frame == 3'b0 || last_frame == 3'b011) begin
-							conf_data <= 8'h54;
-						end else if (last_frame == 3'b1 || last_frame == 3'b100) begin
 							conf_data <= 8'h7F;
-						end else begin
+						end else if (last_frame == 3'b1 || last_frame == 3'b100) begin
 							conf_data <= 8'h2A;
+						end else begin
+							conf_data <= 8'h54;
 						end
-						
-						
+
+
 						//case(exposure)
 						//	2'b00: begin
 						//		conf_data <= 8'h54;  //MID
 						//		exposure <= 2'b01;
 						//	end
-						//	
+						//
 						//	2'b01: begin
 						//		conf_data <= 8'h7F;  //HIGH
 						//		exposure <= 2'b10;
 						//	end
-						//	
+						//
 						//	2'b10: begin
 						//		conf_data <= 8'h2A;  //LOW
 						//		exposure <= 2'b0;
@@ -104,10 +104,10 @@ module camera_config(
 						config_start <= 1'b0;
 					end
 				end
-				
+
 				//WAIT_STATE: begin
 				//	STATE <= CHANGE_EXP2;
-				//	
+				//
 				//	//if(done) begin
 				//	//	if(exposure == 2'b0) begin
 				//	//		conf_data <= 8'h55;
@@ -123,7 +123,7 @@ module camera_config(
 				//	//	config_start <= 1'b0;
 				//	//end
 				//end
-				
+
 				//CHANGE_EXP2: begin
 				//	if(done) begin
 				//		case(exposure)
@@ -131,18 +131,18 @@ module camera_config(
 				//				conf_data <= 8'h3;  //MID
 				//				exposure <= 2'b01;
 				//			end
-				//			
+				//
 				//			2'b01: begin
 				//				conf_data <= 8'h0;  //HIGH
 				//				exposure <= 2'b10;
 				//			end
-				//			
+				//
 				//			2'b10: begin
 				//				conf_data <= 8'h3;  //LOW
 				//				exposure <= 2'b0;
 				//			end
-				//		
-				//		
+				//
+				//
 				//		endcase
 				//		conf_addr <= 8'h04;
 				//		STATE <= IDLE;
@@ -152,16 +152,16 @@ module camera_config(
 				//	end
 				//end
 			endcase
-		end	
+		end
 	end
-	
-	
+
+
 	ov7670_rom ov7670_rom (
 		.clk (clk_25M),
 		.address (rom_address),
 		.dout (rom_data)
 	);
-	
+
 	ov7670_config ov7670_config(
 		.clk_25M (clk_25M),
 		.rst_n (rst_n),
@@ -176,7 +176,7 @@ module camera_config(
 		.sccb_data (sccb_data),
 		.sccb_address (sccb_address)
 	);
-	
+
 	sccb sccb(
 		.clk_25M (clk_25M),
 		.sccb_start (sccb_start),
@@ -187,6 +187,6 @@ module camera_config(
 		.scl (scl),
 		.ready (sccb_ready)
 	);
-	
+
 endmodule
 
