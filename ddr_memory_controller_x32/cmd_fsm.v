7009614638f62cacd2f9975f7a6b612d2a7e9191
Markos Stefanidis <markos.stefanidhs@gmail.com>
2022-02-14 19:00:36 +0200

    Fix artifact issue

--- ddr_memory_controller_x32/cmd_fsm.v
+++ ddr_memory_controller_x32/cmd_fsm.v
@@ -21,7 +21,7 @@ module cmd_fsm(
 
 // This module Decodes DDR commands after the initilization sequence is complete.
 
-// Busy is asserted a few cycles before the auto refresh command needs to be executed. 
+// Busy is asserted a few cycles before the auto refresh command needs to be executed.
 // Otherwise, read/write requests that happened on the same cycle would be lost.
 
 // read is asserted so that the datapath module knows when it needs to read data from dq pins.
@@ -43,14 +43,14 @@ module cmd_fsm(
 	localparam AUTO_REF = 13;
 	localparam AUTO_REF_COUNTER = 14;
 	localparam TIMER = 15;
-	
+
 	reg [3:0] RETURN_STATE;
 	reg [2:0] reg_cmd;
 	reg [15:0] counter; //1 cycle delay
 	reg [10:0] ref_counter;
 	reg ref_reset;
 	reg read_assert;
-	
+
 	always @ (posedge clk) begin
 		if(~rst_n) begin
 			STATE <= 4'b0;
@@ -59,11 +59,12 @@ module cmd_fsm(
 			ref_reset <= 1'b0;
 			burst_counter <= 1'b1;
 			read_assert <= 1'b0;
+			write <= 1'b0;
 		end else if (init_done) begin
-			
+
 			case(STATE)
-				
-				IDLE: begin 
+
+				IDLE: begin
 					if (ref_counter > 11'd1020 && ref_counter <= 11'd1030) begin
 						busy <= 1'b1;
 						if (cmd_valid) begin
@@ -78,7 +79,7 @@ module cmd_fsm(
 									(cmd == 4'b0110) ? LMR :
 									(cmd == 4'b0111) ? SREF_ENTER :
 									IDLE;
-									
+
 							counter <= 15'h2; //LMR State as counter, tMRD 2 cycles + 1 for state change
 						end
 					end else if (ref_counter > 11'd1030) begin
@@ -99,16 +100,16 @@ module cmd_fsm(
 									(cmd == 4'b0110) ? LMR :
 									(cmd == 4'b0111) ? SREF_ENTER :
 									IDLE;
-	
+
 							busy <= (cmd == 4'b0001 || cmd == 4'b0010 || cmd == 4'b0011 || cmd == 4'b0100 ||
 									cmd == 4'b0101 || cmd == 4'b0110 || cmd == 4'b0111);
 							counter <= 15'h2; //LMR State as counter tMRD 2 cycles + 1 for state change
 						end else begin
 							busy <= 1'b0;
 						end
-					end 
+					end
 				end
-				
+
 				ACT: begin //ACT
 					STATE <= TIMER; //TIMER
 					if (reg_cmd[2:0] == 3'b001) begin
@@ -122,30 +123,30 @@ module cmd_fsm(
 					end
 					counter <= 16'h2; //20ns (20*10^-9 * 133.33*10^6Hz = 2.666)
 				end
-				
+
 				READ: begin
-					read_assert <= 1'b1;				
+					read_assert <= 1'b1;
 					STATE <= TIMER;
 					RETURN_STATE <= RD_DATA;
 					counter <= 16'h0; //CAS Latency = 2
 				end
-				
+
 				WRITE: begin
 					STATE <=  WR_DATA;
 				end
-				
+
 				READ_PRE: begin
 					read_assert <= 1'b1;
 					STATE <= TIMER;
 					RETURN_STATE <= RD_DATA;
 					counter <= 16'h1; //CAS Latency = 2
 				end
-				
+
 				WRITE_PRE: begin
 					STATE <=  WR_DATA;
 				end
 
-				
+
 				RD_DATA: begin //RD_DATA
 					read_assert <= 1'b0;
 					STATE <= (burst_counter == 1'b0) ? (reg_cmd[1] ? TIMER : IDLE) : RD_DATA; //reg_cmd[1] --> PRECHARGE
@@ -153,7 +154,7 @@ module cmd_fsm(
 					RETURN_STATE <= IDLE;
 					counter <=  16'h2; // 20ns (20*10^-9 * 133.33*10^6Hz = 2.666)
 				end
-				
+
 				WR_DATA: begin
 					STATE <= (burst_counter == 1'b0) ? (reg_cmd[2] ? TIMER : IDLE) : WR_DATA; //reg_cmd[2] --> PRECHARGE
 					RETURN_STATE <= IDLE;
@@ -161,7 +162,7 @@ module cmd_fsm(
 					burst_counter <= burst_counter - 1;
 					counter <= 16'h2; // 20ns (20*10^-9 * 133.33*10^6Hz = 2.666)
 				end
-				
+
 				PWRDWN_ENTER: begin
 					if (cmd_valid) begin
 						reg_cmd <= cmd;
@@ -174,19 +175,19 @@ module cmd_fsm(
 						busy <= 1'b0;
 					end
 				end
-				
+
 				PWRDWN_EXIT: begin
 					STATE <= IDLE;
 				end
-				
+
 				LMR: begin
 					counter <= counter - 1;
-					STATE <= (counter == 16'b0) ? IDLE : LMR; 
+					STATE <= (counter == 16'b0) ? IDLE : LMR;
 					busy <= (counter != 16'b0);
 				end
-				
+
 				SREF_ENTER: begin
-				
+
 					if (cmd_valid) begin
 						reg_cmd <= cmd;
 						if(cmd == 4'b0001 || cmd == 4'b0011) begin
@@ -197,7 +198,7 @@ module cmd_fsm(
 						end else if (cmd[3] || cmd == 4'b000) begin
 							busy <= 1'b0;
 							STATE <= SREF_ENTER;
-							
+
 						end else begin
 							busy <= 1'b1;
 							STATE <= SREF_EXIT;
@@ -213,43 +214,43 @@ module cmd_fsm(
 						busy <= 1'b0;
 						STATE <= SREF_ENTER;
 					end
-					
+
 				end
-				
+
 				SREF_EXIT: begin
 					STATE <= TIMER;
 				end
-				
+
 				AUTO_REF: begin
 					STATE <= AUTO_REF_COUNTER;
 				end
-				
-				
+
+
 				AUTO_REF_COUNTER: begin
 					counter <= counter - 1;
 					STATE <= (counter == 16'b0) ? IDLE : AUTO_REF_COUNTER;
 					busy <= (counter != 16'b0);
-					ref_reset <= (counter != 16'b0);					
+					ref_reset <= (counter != 16'b0);
 				end
-				
-				
-				
+
+
+
 				TIMER: begin
 					write <= (counter == 16'b0) ? (RETURN_STATE == WRITE || RETURN_STATE == WRITE_PRE) : 1'b0;
 					counter <= counter - 1;
 					STATE <= (counter == 16'b0) ? RETURN_STATE : TIMER;
 				end
 			endcase
-		
-			ref_counter <= (ref_reset) ? 11'b0 : ref_counter + 1; 
-			
+
+			ref_counter <= (ref_reset) ? 11'b0 : ref_counter + 1;
+
 		end else begin
 			busy <= 1'b1;
 		end
-	
+
 	end
-	
+
 	assign read = (read_assert) && ~(STATE == RD_DATA);
-	
+
 
 endmodule
--- ddr_memory_controller_x32/ddr_memory_controller.v
+++ ddr_memory_controller_x32/ddr_memory_controller.v
@@ -6,11 +6,11 @@ module ddr_memory_controller(
 	input [3:0] cmd,
 	input cmd_valid,
 	input sys_200us,
-	
+
 	output [127:0] rd_data,
 	output cmd_busy,
 	output init_done,
-	
+
 	output read_data_valid,
 	output [1:0] ddr_clk,
 	output [1:0] ddr_cke,
@@ -24,29 +24,28 @@ module ddr_memory_controller(
 	output [3:0] ddr_dm,
 	inout [3:0] ddr_dqs
 );
-	
-	// This DDR Controller is designed to work for a 32bit memory with burst_term = 4 and CAS latency = 2. 
+
+	// This DDR Controller is designed to work for a 32bit memory with burst_term = 4 and CAS latency = 2.
 	// You could change the burst term and CAS latence through a LMR command but it would not work, as the wr_data and the rd_data are fixed to 128bit and CAS latency to 2.
-	
-	
-	
+
+
+
 	wire [3:0] c_state;
 	wire [3:0] i_state;
+	wire prmbdet;
 	reg [127:0] data_out;
 	wire read;
 	reg init_start;
 	reg data_valid;
 	reg [127:0] reg_data_in;
-	
-	wire burst_counter;
 
-	
-	
+	wire [1:0] burst_counter;
+
 	wire write;
-	
-	
-	
-	
+
+
+
+
 	always @ (posedge clk) begin
 		if(~rst_n) begin
 			reg_data_in <= 128'b0;
@@ -56,7 +55,7 @@ module ddr_memory_controller(
 			init_start <= ~sys_200us;
 		end
 	end
-	
+
 	ram_initialize init(   // Module responsible for the initialization of DDR memory. The memory is initialized at reset.
 		.clk (clk),
 		.rst_n (rst_n),
@@ -66,7 +65,7 @@ module ddr_memory_controller(
 		.STATE (i_state),
 		.cke (cke)
 	);
-	
+
 	cmd_fsm cmd_fsm(      // Module responsible for decoding DDR commands.
 		.clk (clk),
 		.rst_n (rst_n),
@@ -79,7 +78,7 @@ module ddr_memory_controller(
 		.write (write),
 		.burst_counter (burst_counter)
 	);
-	
+
 	signal_generate signal(  // This module generates the command and address signals for the DDR memory.
 		.clk (clk),
 		.rst_n (rst_n),
@@ -101,17 +100,13 @@ module ddr_memory_controller(
 	reg uddcntl; // When low for 2 cycles, the delay of the DQSDLL is updated. As the Lattice High-Speed I/O Interface guide states, it should be asserted at the begining of READ commands.
 				 // https://www.latticesemi.com/view_document?document_id=21646
 	reg [1:0] uddcntl_counter;
-	
-	
+
+
 	reg [31:0] dataout_n; // dataout_n is the data to be written on the negative of the clock
 	reg [31:0] dataout_p; // dataout_p is the data to be written on the possitive of the clock
 	wire [31:0] data_tri_n; //data_tri_n and data_tri_p need to be asserted high for the datapath to know that dq pins is acting as an output
 	wire [31:0] data_tri_p;
-	
-	reg dqs_en_n;
-	
-	
-	
+
 	always@(posedge clk) begin
 		if(~rst_n) begin
 			uddcntl <= 1'b1;
@@ -121,142 +116,165 @@ module ddr_memory_controller(
 				uddcntl <= (uddcntl_counter != 2'b01) ? 1'b0 : 1'b1;
 				uddcntl_counter <= (uddcntl_counter != 2'b01) ? uddcntl_counter + 1 : 2'b01;
 			end
-		end		
+		end
 	end
 
 
 	reg write_counter;
 
+	reg q_write;
+
+	wire dqs_en_n;
+
 	always@(posedge clk) begin
 		if(~rst_n) begin
 			dataout_n <= 32'b0;
 			dataout_p <= 32'b0;
-			
-			dqs_en_n <= 1'b1;
-			
+
+			//dqs_en_n <= 1'b1;
+
 			write_counter <= 1'b1;
-			
+
+			q_write <= 1'b0;
 		end else begin
+
+ 		q_write <= write;
+
 			if(write) begin  //WR_DATA
-				
-				dqs_en_n <= 1'b0;
-				
-				
+
+				//dqs_en_n <= 1'b0;
+
 				case(write_counter)
-					
+
 					1'b1: begin
 						{dataout_p, dataout_n}  <= reg_data_in[127:64];
 						write_counter <= write_counter - 1;
 					end
-					
+
 					1'b0: begin
 						{dataout_p, dataout_n}  <= reg_data_in[63:0];
 						write_counter <= write_counter - 1;
 					end
-					
+
 				endcase
 			end else begin
-				dqs_en_n <= 1'b1;			
+				//dqs_en_n <= 1'b1;
 			end
-			
-		end		
+
+		end
 	end
-	
+
+	assign dqs_en_n = ~(write || q_write);
+
 	assign data_tri_n = (dqs_en_n) ? 32'hFFFFFFFF : 32'h0;
 	assign data_tri_p = (dqs_en_n) ? 32'hFFFFFFFF : 32'h0;
-	
+
 	wire start_read;
 	wire [3:0] datapath_valid;
 	reg [3:0] q_datapath_valid;
 	wire [3:0] dqs_tri_n;
 	wire [3:0] dqs_tri_p;
-	
+
 	assign dqs_tri_p = {dqs_en_n, dqs_en_n, dqs_en_n, dqs_en_n}; //same with data_tri signals, dqs_tri signals need to be asserted to high when the dqs pins act as an output
 	assign dqs_tri_n = {dqs_en_n, dqs_en_n, dqs_en_n, dqs_en_n};
-	
+
 	wire [31:0] datain_n; //datain_n is the data read at the negative edge of the clock
 	wire [31:0] datain_p; //datain_p is the data read at the possitive edge of the clock
-	
-	reg read_counter;
 
-	
+	reg read_counter;
+	reg q_start_read;
+	reg q_read;
+	reg qq_read;
+	reg qqq_read;
+	reg qqqq_read;
+
 	always@(posedge clk) begin
 		if(~rst_n) begin
 			q_datapath_valid <= 1'b0;
 			data_out <= 128'b0;
 			data_valid <= 1'b0;
 			read_counter <= 1'b1;
+			q_start_read <= 1'b0;
+			q_read <= 1'b0;
+			qq_read <= 1'b0;
+			qqq_read <= 1'b0;
+			qqqq_read <= 1'b0;
+
 		end else begin
-			
+
 			q_datapath_valid <= datapath_valid;
-			
-					
+			q_read <= read;
+			qq_read <= q_read;
+			qqq_read <= qq_read;
+			qqqq_read <= qqq_read;
+
+
 			if(start_read) begin
 				read_counter <= 1'b0;
 				data_valid <= 1'b0;
 				data_out[127:96] <= datain_p;
 				data_out[95:64] <= datain_n;
 			end else begin
-							
+
 				if (read_counter != 1'b1) begin
 					read_counter <= read_counter - 1;
 				end
-				
-				
+
+
 				case(read_counter)
-				
+
 					1'b0: begin
 						data_out[63:32] <= datain_p;
 						data_out[31:0] <= datain_n;
 						data_valid <= 1'b1;
 					end
-					
+
 					default: data_valid <= 1'b0;
-					
+
 				endcase
 			end
 
 		end
 	end
-	
-	assign start_read = ((&datapath_valid)^(&q_datapath_valid)) && (&datapath_valid); //datapath_valid will go high when data read are valid, but will not go low until another read command is executed
-	assign rd_data = data_out;
+
+	assign start_read = &(datapath_valid) && ~(&q_datapath_valid);
+ 	assign rd_data = data_out;
 	assign read_data_valid = data_valid;
-	
+
 	wire reset;
 	assign reset = ~rst_n;
-	
-	wire [3:0] datapath_read;
-	
-	assign datapath_read = {read, read, read, read};
-	
 
-	
+	wire [3:0] datapath_read;
+
+	assign datapath_read = {read, read, read, read};
+
+
+
 	datapath datapath(     //This module is generated by ipExpress in Lattice Diamond and contains the ddr buffers and DLLs needed to read/write from memory
 		.clk (clk),
 		.reset (reset),
 		.uddcntl (uddcntl),
 		.read (datapath_read),
-		
+
 		.dataout_p (dataout_p),
 		.dataout_n (dataout_n),
 		.datatri_p (data_tri_p),
 		.datatri_n (data_tri_n),
 		.dqstri_p (dqs_tri_p),
 		.dqstri_n (dqs_tri_n),
-		
+
 		.datain_p (datain_p),
 		.datain_n (datain_n),
 		.dqsc (),
-		.prmbdet (),
+		.prmbdet (prmbdet),
 		.lock (),
-		
+
 		.datavalid (datapath_valid),
 		.dq (ddr_dq),
 		.dqs (ddr_dqs),
 		.ddrclk (ddr_clk)
 	);
-	
+
 	assign ddr_dm = 4'b0;
 
 
